248a32ba86aff77de53594a403bb7621c1a8a273
SamGodwin2 <sam.godwin@ext.ons.gov.uk>
2019-06-10 09:11:59 +0100

    Bumped the author dynamodb module version to 0.2

--- author.tf
+++ author.tf
@@ -486,7 +486,7 @@ module "author-survey-runner-dynamodb" {
 }
 
 module "author-dynamodb" {
-  source              = "github.com/ONSdigital/eq-author-terraform-dynamodb?ref=v0.1"
+  source              = "github.com/ONSdigital/eq-author-terraform-dynamodb?ref=v0.2"
   env                 = "${var.env}-author"
   aws_account_id      = "${var.aws_account_id}"
   aws_assume_role_arn = "${var.aws_assume_role_arn}"
